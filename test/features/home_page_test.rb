926634f2adedd526c4630037a3686408d60e4f55
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-09 10:29:27 -0700

    tests sprint.ly link. Fixes #12

--- test/features/home_page_test.rb
+++ test/features/home_page_test.rb
@@ -5,5 +5,9 @@ describe "The static home page" do
     visit "http://localhost:3000"
     page.text.must_include "Home"
   end
+  it "has a functioning title" do
+    visit "http://localhost:3000"
+    page.text.must_include "Portfolio"
+  end
 end
 
