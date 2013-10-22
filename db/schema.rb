e6cf8ea7fc0f5562af8f4a2a57c3abdea9d43e5a
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-22 15:05:38 -0700

    migrations for user: name/provider/uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131021223340) do
+ActiveRecord::Schema.define(:version => 20131022215607) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(:version => 20131021223340) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
