74c0c081b55e7a22f8162c8901791aea0314b355
Ivan Storck <ivanoats@gmail.com>
2013-10-23 11:38:52 -0700

    fixed comment approval button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,16 +13,17 @@ class CommentsController < ApplicationController
     end
   end
 
-  # def update
-  #   @comment = Comment.find(params[:id])
-  #   if @comment.update_attributes(params[:comment])
-  #     flash[:notice] = "Comment Approved!"
-  #     redirect_to post_path(@comment.post_id)
-  #   else
-  #     flash[:alert] = "Comment not approved."
-  #     redirect_to post_path(@comment.post_id)
-  #   end
-  # end
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(params[:comment])
+      flash[:notice] = "Comment Approved!"
+      redirect_to @comment.post
+    else
+      flash[:alert] = "Comment not approved."
+      @post = @comment.post
+      render template: "posts/show"
+    end
+  end
 
   # def destroy
   #   @comment = Comment.find(params[:id])
