efd678ee1c473f8ce27d0d89e57fdcde8c77c67f
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-17 17:11:35 -0800

    commentable_id accessible

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   attr_accessible :approved, :author, :author_email,
                   :author_url, :content, :referrer,
-                  :user_agent, :user_ip
+                  :user_agent, :user_ip, :commentable_id
   belongs_to :commentable, polymorphic: true
 end
